b0c85d4a59f747c3b136f7548a761d7319c1a189
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-11-01 00:30:44 +0700

    Add files via upload

---
+++ PBO10119042Latihan18kelipatan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+/**
+ *
+ * @author Muhammad RIfqu Abdillah
+ *  Nama : Muhammad Rifqu Abdillah
+ * Kelas: Pbo-IF1
+ * Nim : 10119042   
+ */
+public class IF110119042Latihan18kelipatan {
+    public static void main(String[] args){
+      
+        for(double i = 3.5; i<=35; i += 3.5){
+         System.out.println("i: " +i);
+        }
+    }
+    
+}
